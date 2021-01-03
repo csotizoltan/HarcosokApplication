cf65a1f8aa41a563aa9d2f0659c3ff302b4267d5
Zoltan Csoti <csoti.zoltan@gmail.com>
2021-01-03 12:05:55 +0100

    Minden funkció működik

--- HarcosokApplication/Form1.cs
+++ HarcosokApplication/Form1.cs
@@ -30,13 +30,12 @@ namespace HarcosokApplication
             }
             catch (MySqlException ex)
             {
-                MessageBox.Show(ex.Message + "\nA program leáll.", "Adatbázis kapcsolódási hiba!", 
+                MessageBox.Show(ex.Message + "\nA program leáll.", "Adatbázis kapcsolódási hiba!",
                     MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 this.Close();
             }
-            
+
             ListHarcosok();
-            ListKepessegek();
 
         }
 
@@ -90,7 +89,7 @@ namespace HarcosokApplication
             }
 
             int harcosIndex = harcosokListBox.SelectedIndex + 1;
-            
+
             sql.CommandText = "SELECT id, nev, leiras, harcos_id FROM `kepessegek` WHERE harcos_id = @harcos_index";
             sql.Parameters.AddWithValue("@harcos_index", harcosIndex);
 
@@ -111,27 +110,170 @@ namespace HarcosokApplication
         }
 
 
-        private void buttonHarcosLetrehozasa_Click(object sender, EventArgs e)
+        // ------- Harcos létrehozása -------
+
+        private void CreateWarrior()
+        {
+            string nev = harcosNeveTextBox.Text.Trim();
+            if (string.IsNullOrWhiteSpace(nev))
+            {
+                MessageBox.Show("Add meg a harcos nevét!",
+                    "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                harcosNeveTextBox.Focus();
+                return;
+            }
+
+            sql = conn.CreateCommand();
+            sql.CommandText = "SELECT COUNT(*) FROM harcosok WHERE nev = @nev";
+            sql.Parameters.AddWithValue("@nev", nev);
+            long exist = (long)sql.ExecuteScalar();
+
+            if (exist > 0)
+            {
+                MessageBox.Show("Ilyen néven már létezik harcos!",
+                    "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                harcosNeveTextBox.Focus();
+                return;
+            }
+
+            sql.Parameters.Clear();
+
+            sql.CommandText = @"INSERT INTO harcosok (nev, letrehozas) 
+                                VALUES (@harcos_nev, current_timestamp());";
+
+            sql.Parameters.AddWithValue("@harcos_nev", nev);
+            sql.ExecuteNonQuery();
+
+            harcosokListBox.Items.Clear(); // Törlöm a harcosokListBox tartalmát
+            ListHarcosok(); // majd újra kiolvasom és megjelenítem a harcosokat a listában
+        }
+
+
+        // ------- Képesség létrehozása -------
+
+        private void CreateCapability()
+        {
+            if (hasznaloComboBox.SelectedIndex < 0)
+            {
+                MessageBox.Show("Először válassz ki egy harcost!",
+                    "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string kepessegNeve = kepessegNeveTextBox.Text.Trim();
+            if (string.IsNullOrWhiteSpace(kepessegNeve))
+            {
+                MessageBox.Show("Add meg a képesség nevét!",
+                    "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                kepessegNeveTextBox.Focus();
+                return;
+            }
+
+            string kepesseg_leiras = leirasTextBox.Text.Trim();
+            int harcosIndex = hasznaloComboBox.SelectedIndex + 1;
+
+            sql.CommandText = @"INSERT INTO kepessegek (nev, leiras, harcos_id) 
+                                VALUES (@kepesseg_nev, @kepesseg_leiras, @harcos_id);";
+
+            sql.Parameters.AddWithValue("@kepesseg_nev", kepessegNeve);
+            sql.Parameters.AddWithValue("@kepesseg_leiras", kepesseg_leiras);
+            sql.Parameters.AddWithValue("@harcos_id", harcosIndex);
+            sql.ExecuteNonQuery();
+
+            MessageBox.Show("A(z) " + hasznaloComboBox.SelectedItem.ToString() + " harcoshoz a " +
+                kepessegNeve + " képesség hozzáadva lett.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        // ------- Képesség törlése -------
+
+        private void DeleteCapability()
+        {
+            if (kepessegekListBox.SelectedIndex < 0)
+            {
+                MessageBox.Show("Válaszd ki, hogy mely képességet akarod törölni!",
+                    "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int harcosIndex = ((Kepessegek)kepessegekListBox.SelectedItem).Id;
+
+            try
+            {
+                sql.CommandText = "DELETE FROM kepessegek WHERE id = @id";
+                sql.Parameters.AddWithValue("@id", harcosIndex);
+                sql.ExecuteNonQuery();
+
+                MessageBox.Show("A " + ((Kepessegek)kepessegekListBox.SelectedItem).Nev +
+                    " képesség törölve lett.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            kepessegekListBox.Items.Clear(); // Törlöm a kepessegekListBox tartalmát
+            ListKepessegek(); // majd újra kiolvasom és megjelenítem a képességet a listában
+        }
+
+
+        // ------- Képesség módosítása -------
+
+        private void UpdateCapability()
         {
 
+            if (kepessegekListBox.SelectedIndex < 0)
+            {
+                MessageBox.Show("Válaszd ki, hogy mely képességet akarod módosítani!",
+                    "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string kepesseg = kepessegLeirasaTextBox.Text.Trim();
+            int kepessegIndex = ((Kepessegek)kepessegekListBox.SelectedItem).Id;
+            string kepessegLeiras = kepessegLeirasaTextBox.Text.Trim();
+
+            try
+            {
+                sql.CommandText = "UPDATE kepessegek SET leiras = @kepessegLeiras WHERE id = @id";
+
+                sql.Parameters.AddWithValue("@kepessegLeiras", kepessegLeiras);
+                sql.Parameters.AddWithValue("@id", kepessegIndex);
+                sql.ExecuteNonQuery();
+
+                MessageBox.Show("A " + ((Kepessegek)kepessegekListBox.SelectedItem).Nev + " képesség leírása módosítva lett.",
+                    "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            ListKepessegek();
+        }
+
+
+        private void buttonHarcosLetrehozasa_Click(object sender, EventArgs e)
+        {
+            CreateWarrior();
         }
 
 
         private void buttonKepessegLetrehozasa_Click(object sender, EventArgs e)
         {
-
+            CreateCapability();
         }
 
 
         private void buttonKepessegTorol_Click(object sender, EventArgs e)
         {
-            
+            DeleteCapability();
         }
 
 
         private void buttonKepessegLeirasModosit_Click(object sender, EventArgs e)
         {
-            
+            UpdateCapability();
         }
 
 
@@ -143,7 +285,7 @@ namespace HarcosokApplication
 
         private void hasznaloComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+
         }
 
 
